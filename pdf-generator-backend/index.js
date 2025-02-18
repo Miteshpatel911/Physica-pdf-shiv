a9c96d62664b38870fe838bfd9fc5c2552e58ea0
Mitesh <55597760+Miteshpatel911@users.noreply.github.com>
2025-02-18 14:53:51 +0530

    Updated for Neuro Report

--- pdf-generator-backend/index.js
+++ pdf-generator-backend/index.js
@@ -54,6 +54,7 @@ app.post('/generate-pdf', (req, res) => {
 
   const scaleFactor = 1.05;
   doc.font('./assets/PlusJakartaSans-SemiBold.ttf');
+  //doc.fillColor('#969696');
 
   // Add the header text
   doc.fontSize(8 * scaleFactor).fillColor('#333').text(`Name: ${data.patient_name}`, 15, 97);
@@ -64,8 +65,8 @@ app.post('/generate-pdf', (req, res) => {
   doc.fontSize(8 * scaleFactor).text(`Patient ID: 1012`, 202, 117); // Add Patient ID dynamically if available
   doc.fontSize(8 * scaleFactor).text(`Date Of Report: 11/01/2025`, 388, 117);
 
-  doc.fontSize(8 * scaleFactor).text(`Surgery Type: ${data.surgery_type}`, 15, 138, { width: 170 });
-  doc.fontSize(8 * scaleFactor).text(`Date Of Surgery: ${data.date_of_surgery}`, 202, 138);
+  // doc.fontSize(8 * scaleFactor).text(`Surgery Type: ${data.surgery_type}`, 15, 138, { width: 170 });
+  // doc.fontSize(8 * scaleFactor).text(`Date Of Surgery: ${data.date_of_surgery}`, 202, 138);
 
   // Horizontal line Section 1
   doc.strokeColor('#FF9318');
@@ -78,15 +79,15 @@ app.post('/generate-pdf', (req, res) => {
   doc.fontSize(8 * scaleFactor).text(data.primary_complaint, 15, 206, { width: 550 });
 
   // Diagnosis
-  doc.fontSize(10).text('Differential Diagnosis', 15, 246);
+  doc.fontSize(10).text('Diagnosis', 15, 246);
   doc.fontSize(8 * scaleFactor).text(data.differential_diagnosis, 15, 265, { width: 170 });
 
   // Pain Level and Pain Type
   doc.fontSize(10).text(`Pain Level: ${data.pain_level}/10`, 202, 246);
   doc.image(painLevelImagePath, 202, 265, { width: 135 });
 
-  doc.fontSize(10).text('Pain Type', 388, 246);
-const painTypeText = data.pain_type.join(', '); // Join the pain_type values with a comma
+  doc.fontSize(10).text('Neurological Symptoms', 388, 246);
+const painTypeText = data.neurological_symptoms.join(', '); // Join the pain_type values with a comma
 doc.fontSize(8 * scaleFactor).text(painTypeText, 388, 265, { width: 170 }); // Render the joined string on one line
 
  // Horizontal line Section 2
@@ -98,15 +99,15 @@ doc.fontSize(8 * scaleFactor).text(painTypeText, 388, 265, { width: 170 }); // R
 
    // Collect physical findings dynamically from the new fields
    const physicalFindings = [
-     `Swelling: ${data.swelling}`,
-     `Tenderness: ${data.tenderness}`,
-     `Redness: ${data.redness}`,
-     `Warmth: ${data.warmth}`,
-     `Tightness: ${data.tightness}`,
-     `Contracture: ${data.contracture}`,
-     `Deformity: ${data.deformity}`,
+     `Affected Limbs: ${data.affected_limbs}`,
+     `Muscle Strength: ${data.muscle_strength}`,
+     `Reflexes: ${data.reflexes}`,
+     `ROM(Active): ${data.range_of_motion_active}`,
+     `ROM(Passive): ${data.range_of_motion_passive}`,
+     `Muscle Tone: ${data.muscle_tone}`,
+     `Dermatome Level: ${data.dermatome_level}`,
+     `Myotome Level: ${data.myotome_level}`,
      `Crepitus: ${data.crepitus}`,
-     `Oedema: ${data.oedema}`,
    ];
  
    const startX = [15, 202, 388]; // X coordinates for the three columns
@@ -125,17 +126,21 @@ doc.fontSize(8 * scaleFactor).text(painTypeText, 388, 265, { width: 170 }); // R
      doc.fontSize(8 * scaleFactor).text(finding, xPos, yPos);
    });
 
-  // Add Positive Special Tests
-  doc.fontSize(10).text('Positive Special Tests', 15, 450).fontSize(10);
-  doc.fontSize(8 * scaleFactor).text('McMurray test - Mild Pain', 15, 472, { width: 170 });// Add dynamically if available
-  // Add Neurological Symptoms
-  doc.fontSize(10).text('Neurological Symptoms', 388, 450);
-  doc.fontSize(8 * scaleFactor).text('None', 388, 472, { width: 170 });// Add dynamically if available
+  // // Add Positive Special Tests
+  // doc.fontSize(10).text('Positive Special Tests', 15, 450).fontSize(10);
+  // doc.fontSize(8 * scaleFactor).text('McMurray test - Mild Pain', 15, 472, { width: 170 });// Add dynamically if available
+  // // Add Neurological Symptoms
+  // doc.fontSize(10).text('Neurological Symptoms', 388, 450);
+  // doc.fontSize(8 * scaleFactor).text('None', 388, 472, { width: 170 });// Add dynamically if available
+
+  doc.fontSize(10).text('Gait Assessment', 15, 413).fontSize(10);
+  const gaitAssessment = data.gait_assessment.join(', '); // Join the pain_type values with a comma
+  doc.fontSize(8 * scaleFactor).text(gaitAssessment, 15, 435, { width: 170 }); // Render the joined string on one line
 
  // Functional Limitations
-doc.fontSize(10).text('Functional Limitations', 202, 450);
-const funlimTypeText = data.functional_limitations.join(', ');
-doc.fontSize(8 * scaleFactor).text(funlimTypeText, 202, 472, { width: 170 });
+// doc.fontSize(10).text('Functional Limitations', 202, 450);
+// const funlimTypeText = data.functional_limitations.join(', ');
+// doc.fontSize(8 * scaleFactor).text(funlimTypeText, 202, 472, { width: 170 });
 
  // Horizontal line Section 3
  doc.moveTo(7, 512).lineTo(587, 512).stroke();  
@@ -157,11 +162,16 @@ doc.fontSize(8 * scaleFactor).text(funlimTypeText, 202, 472, { width: 170 });
   doc.fontSize(8 * scaleFactor).text(data.treatment_plan, 15, 592, { width: 550 });
 
   // Horizontal line Section 3
-  doc.moveTo(7, 658).lineTo(587, 658).stroke(); 
+  doc.moveTo(7, 626).lineTo(587, 626).stroke(); 
 
   // Additional Notes
-  doc.fontSize(10).text('Additional Notes', 15, 670);
-  doc.fontSize(8 * scaleFactor).text(data.doctor_prescription, 15, 687, { width: 550 });
+  doc.fontSize(10).text('Additional Notes', 15, 642);
+  doc.fontSize(8 * scaleFactor).text(data.additional_notes, 15, 658, { width: 550 });
+
+  // Additional Notes
+  doc.fontSize(10).text(`Doctor's Prescription`, 15, 698);
+  doc.fontSize(8 * scaleFactor).text(data.doctor_prescription, 15, 716, { width: 550 });
+  
 
   // Footer
   doc.fontSize(7 * 1.155).text('Physica Healthtech Private Limited', 15, 780);
@@ -172,8 +182,7 @@ doc.fontSize(8 * scaleFactor).text(funlimTypeText, 202, 472, { width: 170 });
   doc.image(callImagePath, 139, 815, { width: 10 });
   doc.fontSize(6 * 1.2).text('+91 9892260450', 153, 815);
 
-  doc.fontSize(8 * scaleFactor).text(`E`, 580, 830);
-
+  // doc.fontSize(8 * scaleFactor).text(`E`, 580, 832);
   doc.end();
 });
 
